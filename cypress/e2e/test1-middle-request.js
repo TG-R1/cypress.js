357b12507984154435521d4dc5f82ff576a4a7af
TG-R1 <test.grinder.qa@gmail.com>
2023-08-23 16:53:32 +0300

    Усложняем запросы

---
+++ cypress/e2e/test1-middle-request.js
@@ -0,0 +1,8 @@
+describe ("[Тестирование] Более сложные запросы", function() {
+    // Задать пред-запрос
+    var host = 'https://pokemonbattle.me/'
+    it('', function (){
+        cy.visit(host)
+
+    })
+})
